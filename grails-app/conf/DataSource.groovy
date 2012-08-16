2cf3458e8cc331cafc4ee8dc719d1070b6fc6559
Igor <igorabiola@gmail.com>
2012-08-15 23:07:16 -0300

    Configs para funcionar do cloudFoundry

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,5 +1,9 @@
 grails { 
   mongo { 
+//	  host = "${cloud.services.mongoDb.connection.host}"
+//	  port = "${cloud.services.mongoDb.connection.port}"
+//	  username = "${cloud.services.mongoDb.connection.username}"
+//	  password="${cloud.services.mongoDb.connection.password}"
     host = "localhost"
     port = 27017 
     username = ""
